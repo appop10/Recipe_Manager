8bc15a3831c20c2db17351bec1a51ece115b3389
appop10 <appoplawski10@gmail.com>
2023-04-24 13:16:53 -0500

    Updated navbar functionality and formatted header

--- public_pages/javascript/main.js
+++ public_pages/javascript/main.js
@@ -5,15 +5,18 @@
 // Scroll changes for the Navbar
 function shrinkNav() {
     if (window.scrollY > 100) {
+        // nav settings
         document.querySelector("nav").style.height = "13vh";
-        document.querySelector("nav").style.backgroundColor = "#0e402d";
+        // nav image settings
         document.querySelector("nav img").style.height = "10vh";
         document.querySelector("nav img").style.bottom = "3vh";
     } else {
+        // nav settings
         document.querySelector("nav").style.height = "61vh";
+        // nav image settings
         document.querySelector("nav img").style.height = "45vh";
         document.querySelector("nav img").style.bottom = "0";
-        document.querySelector("nav").style.backgroundColor = "#0e402dcb";
+
     }
 }
 
