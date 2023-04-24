81dd3a4041d48156fe0f5b92b4c1d50aaee5463d
appop10 <appoplawski10@gmail.com>
2023-04-24 17:18:10 -0500

    Updated nav to have two options

--- public_pages/javascript/main.js
+++ public_pages/javascript/main.js
@@ -6,16 +6,16 @@
 function shrinkNav() {
     if (window.scrollY > 0) {
         // nav settings
-        document.querySelector("nav").style.height = "13vh";
+        document.querySelector("nav.transforming").style.height = "13vh";
         // nav image settings
-        document.querySelector("nav img").style.height = "10vh";
-        document.querySelector("nav img").style.bottom = "3vh";
+        document.querySelector("nav.transforming img").style.height = "10vh";
+        document.querySelector("nav.transforming img").style.bottom = "3vh";
     } else {
         // nav settings
-        document.querySelector("nav").style.height = "61vh";
+        document.querySelector("nav.transforming").style.height = "61vh";
         // nav image settings
-        document.querySelector("nav img").style.height = "45vh";
-        document.querySelector("nav img").style.bottom = "0";
+        document.querySelector("nav.transforming img").style.height = "45vh";
+        document.querySelector("nav.transforming img").style.bottom = "0";
 
     }
 }
