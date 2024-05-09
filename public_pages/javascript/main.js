750575f28a8ca4c9ed7ba312eb28640ac4539508
appop10 <appoplawski10@gmail.com>
2024-05-09 09:30:41 -0500

    make nav fixed at the top for home and contact pages

--- public_pages/javascript/main.js
+++ public_pages/javascript/main.js
@@ -2,33 +2,6 @@
     Nav, header, and other elements shared by other JS pages
  */
 
-// Scroll changes for the Navbar
-function shrinkNav() {
-    if (window.scrollY > 0) {
-        // nav settings
-        document.querySelector("nav.transforming").style.height = "13vh";
-        // nav image settings
-        document.querySelector("nav.transforming img").style.height = "10vh";
-        document.querySelector("nav.transforming img").style.bottom = "3vh";
-        // header padding
-        document.querySelector("header").style.marginTop = "20vh";
-    } else {
-        // nav settings
-        document.querySelector("nav.transforming").style.height = "61vh";
-        // nav image settings
-        document.querySelector("nav.transforming img").style.height = "45vh";
-        document.querySelector("nav.transforming img").style.bottom = "";
-        // header padding
-        document.querySelector("header").style.marginTop = "10vh";
-    }
-}
-
-// event handler
-window.onscroll = () => {
-    if (screen.width > 900) {
-        shrinkNav();
-    }
-}
 // transform the mobile drop down menu
 function dropMenu() {
     document.querySelector("nav ul").classList.toggle("drop-hamburger");
